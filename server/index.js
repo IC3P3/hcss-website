3fb3f4f40ed4a2f48296b1eeb2f50301e83f8d40
IC3P3 <n.eifler@freenet.de>
2022-06-29 07:56:56 +0200

    removed .env; started server programming

--- server/index.js
+++ server/index.js
@@ -2,8 +2,34 @@ const dotenv = require('dotenv').config();
 const http = require('http');
 const express = require('express');
 const socketio =  require('socket.io');
+const nodemailer = require('nodemainer');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const port = dotenv.parsed.SERVER_PORT;
+const smtpServer = dotenv.parsed.SMTP_SERVER_URL;
+const smptServerPort = dotenv.parsed.SMTP_SERVER_PORT;
+const smtpUsername = dotenv.parsed.SMTP_USERNAME;
+
+const transporter = nodemailer.createTransporter({
+    host: smtpServer,
+    port: smptServerPort,
+    secure: smptServerPort === 465,
+    auth: {
+        user
+    }
+});
+
+server.on('error', err => {
+    console.error("[Mail Sent Server] A server error has occurred: " + err);
+});
+
+server.on('close', () => {
+    console.log("[Mail Sent Server] Server has been closed.")
+});
+
+server.listen(port, () => {
+    console.log("[Mail Sent Server] The Server is now listening to port " + port);
+});
