64b07a792f7a05d96e33eda61eb644ed0105bc03
Niklas <n.eifler@freenet.de>
2022-06-29 08:32:45 +0200

    Revert "moved website to /client; added nodejs server with express, socketio and dotenv; added gitignore"
    This reverts commit 8dfa742d4ab26aa7f260be4fc2d186e19f9a8174.

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv').config();
-const http = require('http');
-const express = require('express');
-const socketio =  require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-
