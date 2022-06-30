d3aa4a16a451cdd30c5e57ecfe2c061f1467d7c1
Niklas <n.eifler@freenet.de>
2022-06-30 12:28:50 +0200

    server gets message from client, but can't send mail

--- client/js/contactSender.js
+++ client/js/contactSender.js
@@ -1,5 +1,8 @@
-const socket = io();
+const socket = io('http://localhost:3000', {
+    transports : ['websocket']
+});
 
-function sendContactInformation(email, name, message) {
-    
+function sendContactInformation(name, email, message) {
+    socket.emit('sendContact', name, email, message);
+    alert("Die Kontaktanfrage wurde gesendet.");
 }
