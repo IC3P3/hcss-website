20b93bd3aba65d6b093f4ab6979185135a8d7e38
IC3P3 <n.eifler@freenet.de>
2022-06-30 21:15:59 +0200

    added script for adding events from json file

---
+++ js/eventAdder.js
@@ -0,0 +1,39 @@
+function readJSON(path, id) {
+    fetch(path)
+        .then(result => result.json())
+        .then(data => addContentToId(data, id))
+        .catch((error) => console.error(error));
+}
+
+function addContentToId(data, id) {
+    const element = document.getElementById(id);
+    let innerOut = ``;
+
+    data.forEach(obj => {
+        innerOut += 
+        `<div class="row">
+            <div class="col-3">
+                <p>
+                    ${obj.date} <br>
+                    ${obj.time} (${obj.duration})
+                </p>
+            </div>
+            <div class="col-6">
+                <h6>
+                    ${obj.title}
+                </h6>
+                <p>
+                    ${obj.description}
+                </p>
+            </div>
+            <div class="col">
+                <br>
+                ${obj.location}
+            </div>
+        </div>
+        <div class="horizontal-line"></div>
+        <br><br>`
+    });
+
+    element.innerHTML = innerOut;
+}
