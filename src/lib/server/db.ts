11537b7db6f4fb17f884baf5b6edc38db6b5c629
IC3P3 <n.eifler@freenet.de>
2024-10-23 18:53:22 +0200

    Fixed 'ModelNotInitializedError'

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,11 +2,18 @@ import { Sequelize } from 'sequelize-typescript';
 
 import { DATABASE_LOCATION } from '$env/static/private';
 
+import Event from './models/Event';
+import Media from './models/Media';
+import HomeContent from './models/HomeContent';
+
 const sequelize = new Sequelize({
 	dialect: 'sqlite',
 	storage: DATABASE_LOCATION,
-	models: [import.meta.dirname + '/models'],
 	logging: false
 });
 
-export default sequelize;
+sequelize.addModels([Event, Media, HomeContent]);
+
+sequelize.sync();
+
+export default { sequelize, Event, Media, HomeContent };
