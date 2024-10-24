a3475f9e767abe6dbe172c0b547a8b207f499712
IC3P3 <n.eifler@freenet.de>
2024-10-24 20:04:49 +0200

    Added required to needed form inputs

--- src/routes/admin/create/+page.svelte
+++ src/routes/admin/create/+page.svelte
@@ -1,10 +1,10 @@
 <h1>Einträge hinzufügen</h1>
 
 <h2>Veranstaltung hinzufügen</h2>
-<form method="POST" enctype="multipart/form-data">
+<form method="POST">
 	<label>
 		Titel
-		<input name="title" type="text" />
+		<input name="title" type="text" required />
 	</label>
 	<label>
 		Untertitel
@@ -12,11 +12,11 @@
 	</label>
 	<label>
 		Adresse
-		<input name="address" type="text" />
+		<input name="address" type="text" required />
 	</label>
 	<label>
 		Uhrzeit
-		<input name="time" type="datetime-local" />
+		<input name="time" type="datetime-local" required />
 	</label>
 	<button type="submit" formaction="?/createEvent">Veranstaltung erstellen</button>
 </form>
