ae50286aa6712f3b133c3d0a71473e093a812aca
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-10-25 08:48:22 +0200

    Added possibility to add images

--- src/routes/admin/create/+page.server.ts
+++ src/routes/admin/create/+page.server.ts
@@ -1,12 +1,29 @@
 import type { Actions } from './$types';
 
-import { Event } from '$lib/server/db';
+import { Event, Media } from '$lib/server/db';
 
 export const actions = {
 	createEvent: async ({ request }) => {
 		const data = await request.formData();
 
+		// TODO: Add error handling if creating an entry fails
 		const event = Event.create(Object.fromEntries(data));
+
+		return { success: true };
+	},
+	addImage: async ({ request }) => {
+		const data = await request.formData();
+
+		const image = data.get('image') as File;
+		const arrayBuffer = await image.arrayBuffer();
+		const buffer = Buffer.from(arrayBuffer);
+
+		// TODO: Errorhandling to prevent server crashes when Foreign Key contraint
+		const media = Media.create({
+			subtitle: data.get('subtitle'),
+			image: buffer,
+			event_id: data.get('event_id')
+		});
 
 		return { success: true };
 	}
