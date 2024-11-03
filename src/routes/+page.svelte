a68e160dd645844c0a19e96dad342ffb0beb4c9d
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-03 23:17:00 +0100

    Added events section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import type { PageData } from './$types';
 
 	import Header from '$lib/components/Header.svelte';
+	import Events from '$lib/components/Events.svelte';
 
 	// TODO: Make this modular and not hard coded
 	// NOTE: Maybe by adding the id in the db table HomeContent to the id of the element
@@ -13,3 +14,5 @@
 {#if homeImage}
 	<Header {homeImage} />
 {/if}
+
+<Events events={data.events} />
