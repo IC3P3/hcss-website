bcd1b243f7d0fd8ca0dd5f94b4aecf9f76b7a37f
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-04 21:24:13 +0100

    Added offerings

---
+++ src/lib/components/Offerings.svelte
@@ -0,0 +1,72 @@
+<!-- TODO: Replace placeholder images, at first with a static image as it won't be changed often -->
+<section
+	id="offering"
+	class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 text-center justify-center"
+>
+	<h2 class="text-blue-950 font-extrabold text-4xl">Unsere Angebote</h2>
+	<hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+	<div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+		<div class="bg-white rounded-lg shadow-lg">
+			<img
+				src="https://placehold.co/800?text=Vorgruppe+Bild&font=roboto"
+				alt="Vorgruppe"
+				class="w-full h-64 object-cover rounded-t-lg"
+			/>
+			<div class="p-6">
+				<h3 class="text-blue-950 font-bold text-lg mb-2">Vorgruppe</h3>
+				<p class="text-gray-700 font-semibold">
+					Kinder ab ca. 5 - 6 Jahren (Musikalische Früherziehung, Singen)
+				</p>
+				<br />
+				<p class="text-gray-700">Di. 16:30 - 17:15 Uhr</p>
+			</div>
+		</div>
+		<div class="bg-white rounded-lg shadow-lg">
+			<img
+				src="https://placehold.co/800?text=Konzertchor+Bild&font=roboto"
+				alt="Konzertchor"
+				class="w-full h-64 object-cover rounded-t-lg"
+			/>
+			<div class="p-6">
+				<h3 class="text-blue-950 font-bold text-lg mb-2">Konzertchor</h3>
+				<p class="text-gray-700 font-semibold">Kinder ab ca. 10 Jahren & Erwachsene</p>
+				<p class="text-gray-700">Di. 19:00 - 21:00 Uhr Männerstimmen</p>
+				<p class="text-gray-700">Do. 17:00 - 19:00 Uhr Knaben-, Mädchen- und Frauenstimmen</p>
+				<p class="text-gray-700">Fr. 17:00 - 19:00 Uhr alle Stimmen</p>
+			</div>
+		</div>
+		<div class="bg-white rounded-lg shadow-lg">
+			<img
+				src="https://placehold.co/800?text=Unterricht+Bild&font=roboto"
+				alt="Unterricht"
+				class="w-full h-64 object-cover rounded-t-lg"
+			/>
+			<div class="p-6">
+				<h3 class="text-blue-950 font-bold text-lg mb-2">
+					Gesangsunterricht, Musiktheorie und Klavierunterricht
+				</h3>
+				<p class="text-gray-700">
+					Für Gesangsunterricht, Musiktheorie und Klavierunterricht passen wir uns gerne an deine
+					Zeitplanung an. Kontaktiere uns einfach und wir finden gemeinsam einen passenden Termin.
+				</p>
+			</div>
+		</div>
+		<div class="bg-white rounded-lg shadow-lg">
+			<img
+				src="https://placehold.co/800?text=Projektchor+Bild&font=roboto"
+				alt="Projekte"
+				class="w-full h-64 object-cover rounded-t-lg"
+			/>
+			<div class="p-6">
+				<h3 class="text-blue-950 font-bold text-lg mb-2">Chorprojekte</h3>
+				<p class="text-gray-700">
+					Für interessierte SängerInnen nach Anmeldung und eigener Vorbereitung.
+				</p>
+				<p class="text-gray-700">
+					Üblicherweise werden an einem Probenwochenende und einem Konzertwochenende A Capella Werke
+					für Kammerchor oder das große Oratorienrepertoire erarbeitet und aufgeführt.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
