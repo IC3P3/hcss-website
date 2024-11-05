972f6ce003f1a7367d933a13cad798ea09bb8525
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-05 16:08:44 +0100

    Added media section

---
+++ src/lib/components/Media.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let media: string[];
+</script>
+
+<section
+	id="media"
+	class="max-w-screen-xl mx-auto w-screen p-4 py-6 lg:py-8 text-center justify-center"
+>
+	<h2 class="text-blue-950 font-extrabold text-4xl">Medien</h2>
+	<hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+	<div class="flex flex-wrap justify-between">
+		{#each media as item}
+			<div class="relative overflow-hidden w-full sm:w-1/2 lg:w-1/3 group">
+				<img class="w-full h-64 object-cover" src="/api/content/image?id={item.id}" alt="" />
+				<div
+					class="absolute inset-0 flex items-end bg-blue-700 bg-opacity-75 p-8 opacity-0 group-hover:opacity-100 hover:opacity-100 transition-opacity duration-300"
+				>
+					<div class="flex flex-col items-start w-full">
+						<!-- FIX: Replace placeholder and input concert title -->
+						<h3 class="text-white text-2xl font-semibold mb-2">Placeholder</h3>
+						<span class="text-white text-sm">{item.subtitle}</span>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 
 	import Header from '$lib/components/Header.svelte';
 	import Events from '$lib/components/Events.svelte';
+	import Media from '$lib/components/Media.svelte';
 	import Offerings from '$lib/components/Offerings.svelte';
 	import About from '$lib/components/About.svelte';
 	import Club from '$lib/components/Club.svelte';
@@ -12,6 +13,15 @@
 	// TODO: Make this modular and not hard coded
 	// NOTE: Maybe by adding the id in the db table HomeContent to the id of the element
 	const homeImage = data.content.find((image) => image['homeContent.id'] === 'home')?.id.toString();
+
+	// FIX: Typescript types
+	let media = data.content
+		.filter((image) => image['homeContent.id'].includes('media'))
+		.map((image) => {
+			const cleanImage = { ...image };
+			delete cleanImage['homeContent.id'];
+			return cleanImage;
+		});
 </script>
 
 {#if homeImage}
@@ -19,6 +29,7 @@
 {/if}
 
 <Events events={data.events} />
+<Media {media} />
 <Offerings />
 <About />
 <Club />
