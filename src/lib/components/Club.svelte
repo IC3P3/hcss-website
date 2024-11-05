40bb083469073d35cd8e34d5addbfced185b6224
IC3P3 <n.eifler@freenet.de>
2024-11-05 10:21:31 +0100

    Added Club section

---
+++ src/lib/components/Club.svelte
@@ -0,0 +1,71 @@
+<section
+	id="clubs"
+	class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 text-center justify-center"
+>
+	<h2 class="text-blue-950 font-extrabold text-4xl">Unsere Vereine</h2>
+	<hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+	<div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h3 class="text-blue-950 font-bold text-lg mb-2">Helmstedter Chor- und Singschule e.V.</h3>
+			<div class="flex items-center mb-4">
+				<!-- TODO: Image is missing -->
+				<img alt="" class="w-12 h-12 rounded-full mr-4" />
+				<div>
+					<h4 class="text-gray-800 font-semibold">Max Mustermann</h4>
+					<p class="text-gray-600 text-sm">1. Vorsitzender</p>
+				</div>
+			</div>
+			<p class="text-gray-700">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+				invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+				et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+				Lorem ipsum dolor sit amet.
+			</p>
+			<div class="flex items-center mb-4 mt-16">
+				<!-- TODO: Image is missing -->
+				<img alt="" class="w-12 h-12 rounded-full mr-4" />
+				<div>
+					<h4 class="text-gray-800 font-semibold">Max Mustermann</h4>
+					<p class="text-gray-600 text-sm">2. Vorsitzender</p>
+				</div>
+			</div>
+			<p class="text-gray-700">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+				invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+				et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+				Lorem ipsum dolor sit amet.
+			</p>
+		</div>
+		<div class="bg-white p-6 rounded-lg shadow-lg">
+			<h3 class="text-blue-950 font-bold text-lg mb-2">Föderverein</h3>
+			<div class="flex items-center mb-4">
+				<!-- TODO: Image is missing -->
+				<img alt="" class="w-12 h-12 rounded-full mr-4" />
+				<div>
+					<h4 class="text-gray-800 font-semibold">Max Mustermann</h4>
+					<p class="text-gray-600 text-sm">1. Vorsitzender</p>
+				</div>
+			</div>
+			<p class="text-gray-700">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+				invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+				et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+				Lorem ipsum dolor sit amet.
+			</p>
+			<div class="flex items-center mb-4 mt-16">
+				<!-- TODO: Image is missing -->
+				<img alt="" class="w-12 h-12 rounded-full mr-4" />
+				<div>
+					<h4 class="text-gray-800 font-semibold">Max Mustermann</h4>
+					<p class="text-gray-600 text-sm">2. Vorsitzender</p>
+				</div>
+			</div>
+			<p class="text-gray-700">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+				invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+				et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+				Lorem ipsum dolor sit amet.
+			</p>
+		</div>
+	</div>
+</section>
