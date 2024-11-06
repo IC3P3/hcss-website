1c145583c76c6634d068ba913d6379a89b42def4
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-06 21:21:34 +0100

    Added call to action

---
+++ src/lib/components/CTA.svelte
@@ -0,0 +1,17 @@
+<section
+	id="cta"
+	class="grid columns-2 mx-auto w-full p-4 py-6 lg:py-8 text-center justify-center bg-blue-900"
+>
+	<div class="py-4 justify-center uppercase tracking-wider font-bold flex items-center">
+		<div>
+			<h1 class="text-white text-2xl mb-2">Haben Sie Interesse an unserer Arbeit?</h1>
+		</div>
+		<div class="ml-8">
+			<a
+				href="#events"
+				class="text-black bg-white py-2 px-4 text-sm hover:bg-gray-200 transition-colors"
+				>Unsere nächsten Auftritte</a
+			>
+		</div>
+	</div>
+</section>
