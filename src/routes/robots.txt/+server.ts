0a557f3f0d415e28133a2057d1959bd197309355
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-10 18:28:16 +0100

    Added dynamic robots.txt

---
+++ src/routes/robots.txt/+server.ts
@@ -0,0 +1,19 @@
+import type { RequestEvent } from '../$types';
+
+export async function GET({ request }: RequestEvent) {
+	const host = request.headers.get('host')!;
+
+	const body = robots(host);
+	const response = new Response(body);
+
+	response.headers.set('Cache-Control', 'max-age=0, s-maxage=3600');
+	response.headers.set('Content-Type', 'text/plain');
+
+	return response;
+}
+
+const robots = (host: string) => `User-agent: *
+Disallow: /api/
+Disallow: /admin/
+Sitemap: ${host}/sitemap.xml
+`;
