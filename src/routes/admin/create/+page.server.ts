1524f5e585c5a8991c677b05f86029f5666ca780
IC3P3 <n.eifler@freenet.de>
2024-11-11 22:34:46 +0100

    Replace event id number input with dropdown

--- src/routes/admin/create/+page.server.ts
+++ src/routes/admin/create/+page.server.ts
@@ -1,13 +1,14 @@
-import type { Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 import { Event, Media } from '$lib/server/db';
+import type { EventMinimal } from '$lib/types';
 
 export const actions = {
 	createEvent: async ({ request }) => {
 		const data = await request.formData();
 
 		// FIX: Tell typescript how this object looks
-		let newEvent = Object.fromEntries(data);
+		const newEvent = Object.fromEntries(data);
 		newEvent.time = new Date(newEvent.time).getTime();
 
 		// TODO: Add error handling if creating an entry fails
@@ -32,3 +33,12 @@ export const actions = {
 		return { success: true };
 	}
 } satisfies Actions;
+
+export const load: PageServerLoad = async () => {
+	const events = (await Event.findAll({
+		attributes: ['id', 'title'],
+		raw: true
+	})) satisfies EventMinimal[];
+
+	return { events: events };
+};
