e1aa1440c7cba88abf1066ca6d706b17489b8f4c
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2024-11-12 13:27:42 +0100

    Adjusted the look of events

--- src/lib/components/Events.svelte
+++ src/lib/components/Events.svelte
@@ -10,28 +10,29 @@
 
 	{#each events as event}
 		<div class="my-8 p-6 bg-white shadow-md rounded-lg border border-gray-200">
-			<div class="lg:grid lg:grid-cols-3 gap-8">
-				<!-- Date & Time -->
-				<div class="text-center l g:text-left">
-					<p class="text-xl font-semibold text-blue-950">
+			<div class="lg:grid lg:grid-cols-4 gap-8">
+				<div class="align-middle mt-1">
+					<p class="text-left text-xl font-semibold text-blue-950">
 						{new Date(event.time).toLocaleDateString('de', {
 							day: '2-digit',
 							month: 'long',
 							year: 'numeric'
 						})}
 					</p>
-					<p class="text-lg text-gray-600">
+					<p class="text-left text-lg text-gray-600">
 						{new Date(event.time).toLocaleTimeString('de', { hour: '2-digit', minute: '2-digit' })} Uhr
 					</p>
 				</div>
 
-				<div class="lg:col-span-1 lg:text-center">
+				<div class="lg:col-span-2 lg:text-center">
 					<h3 class="text-2xl font-bold text-blue-950 mb-2">{event.title}</h3>
 					<p class="text-gray-700 text-lg">{event.subtitle}</p>
 				</div>
 
-				<div class="text-center lg:text-left">
-					<p class="text-lg text-gray-700">{event.address}</p>
+				<div class="lg:text-left align-middle">
+					{#each event.address.split(', ') as part}
+						<p class="text-right text-lg text-gray-700 align-right">{part}</p>
+					{/each}
 				</div>
 			</div>
 		</div>
