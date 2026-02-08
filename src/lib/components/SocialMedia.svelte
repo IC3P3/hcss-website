6031afeefdc4fb453c132276b89b464321a08435
IC3P3 <n.eifler@freenet.de>
2026-02-08 21:01:32 +0100

    Added aria labels to Social Media

--- src/lib/components/SocialMedia.svelte
+++ src/lib/components/SocialMedia.svelte
@@ -1,36 +1,44 @@
 <script lang="ts">
 	import Facebook from '$lib/assets/Facebook.svelte';
 	import Instagram from '$lib/assets/Instagram.svelte';
-	import Whatapp from '$lib/assets/Whatapp.svelte';
-	import Youtube from '$lib/assets/Youtube.svelte';
+	import Whatapp from '$lib/assets/WhatsApp.svelte';
+	import Youtube from '$lib/assets/YouTube.svelte';
 </script>
 
 <div class="flex items-center gap-4 text-gray-50">
 	<a
 		href="https://www.instagram.com/helmstedter_chor/"
 		target="_blank"
+		rel="noopener noreferrer"
 		class="transition-colors duration-300 hover:text-gray-400"
+		aria-label="Instagram-Seite der Helmstedter Chor- und Singschule"
 	>
 		<Instagram classes="h-8 w-8" />
 	</a>
 	<a
 		href="https://whatsapp.com/channel/0029Vb7EWUtFnSzFjy8BBI0S"
 		target="_blank"
+		rel="noopener noreferrer"
 		class="transition-colors duration-300 hover:text-gray-400"
+		aria-label="WhatsApp-Kanal der Helmstedter Chor- und Singschule"
 	>
 		<Whatapp classes="h-8 w-8" />
 	</a>
 	<a
 		href="https://youtube.com"
 		target="_blank"
+		rel="noopener noreferrer"
 		class="transition-colors duration-300 hover:text-gray-400"
+		aria-label="YouTube-Seite der Helmstedter Chor- und Singschule"
 	>
 		<Youtube classes="h-8 w-8" />
 	</a>
 	<a
 		href="https://facebook.com"
 		target="_blank"
+		rel="noopener noreferrer"
 		class="transition-colors duration-300 hover:text-gray-400"
+		aria-label="Facebook-Seite der Helmstedter Chor- und Singschule"
 	>
 		<Facebook classes="h-8 w-8" />
 	</a>
