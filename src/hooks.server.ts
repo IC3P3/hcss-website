66f50acdfcb9b6628071c53d46020b3e04b9153b
IC3P3 <n.eifler@freenet.de>
2026-02-09 21:34:54 +0100

    Made the hooks server better

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,50 +1,69 @@
-import { eq, lte } from 'drizzle-orm';
+import { and, eq, gt, lt } from 'drizzle-orm';
 import { db } from '$lib/server/db';
 import { dev } from '$app/environment';
 import type { Handle } from '@sveltejs/kit';
-import { Session } from '$lib/server/models/Session';
+import { Session } from '$lib/server/models/User';
+import {
+	ONE_DAY_IN_MS,
+	THIRTY_MINUTES_IN_MS,
+	THREE_DAYS_IN_MS,
+	THREE_DAYS_IN_S
+} from '$lib/utils/constants';
+
+const cleanup = { lastRun: 0 };
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const SEVEN_DAYS_COOKIE_TTL = 604800;
-	const SINGLE_DB_RETURN = 1;
-	const EMPTY_DB_RETURN = 0;
-
 	const session = event.cookies.get('session');
 
 	if (!session) return await resolve(event);
 
-	await db
-		.delete(Session)
-		.where(lte(Session.lastSeen, new Date().getTime() - SEVEN_DAYS_COOKIE_TTL));
+	const dateNow = Date.now();
+	if (cleanup.lastRun < dateNow - ONE_DAY_IN_MS) {
+		cleanup.lastRun = dateNow;
+		db.delete(Session)
+			.where(lt(Session.lastSeen, dateNow - THREE_DAYS_IN_MS))
+			.catch(() => (cleanup.lastRun = 0));
+	}
 
 	const user = await db
 		.select({
-			userId: Session.userId
+			userId: Session.userId,
+			lastSeen: Session.lastSeen
 		})
 		.from(Session)
-		.where(eq(Session.sessionKey, session))
-		.limit(SINGLE_DB_RETURN);
+		.where(
+			and(eq(Session.sessionKey, session), gt(Session.lastSeen, dateNow - THREE_DAYS_IN_MS))
+		)
+		.limit(1);
 
-	const userInfo = user.length > EMPTY_DB_RETURN ? { name: user[0].userId } : null;
+	const userInfo = user.length > 0 ? { id: user[0].userId } : null;
 
-	await db
-		.update(Session)
-		.set({
-			lastSeen: new Date().getTime()
-		})
-		.where(eq(Session.sessionKey, session));
+	if (!userInfo) {
+		event.cookies.delete('session', {
+			path: '/'
+		});
 
-	event.cookies.set('session', session, {
-		path: '/',
-		httpOnly: true,
-		sameSite: 'strict',
-		secure: !dev,
-		maxAge: SEVEN_DAYS_COOKIE_TTL
-	});
-
-	if (userInfo) {
-		event.locals.user = userInfo;
+		return await resolve(event);
 	}
+
+	if (user[0].lastSeen + THIRTY_MINUTES_IN_MS < dateNow) {
+		await db
+			.update(Session)
+			.set({
+				lastSeen: dateNow
+			})
+			.where(eq(Session.sessionKey, session));
+
+		event.cookies.set('session', session, {
+			path: '/',
+			httpOnly: true,
+			sameSite: 'strict',
+			secure: !dev,
+			maxAge: THREE_DAYS_IN_S
+		});
+	}
+
+	event.locals.user = userInfo;
 
 	return await resolve(event);
 };
