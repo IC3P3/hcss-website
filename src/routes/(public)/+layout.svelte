97cbcd543be6bbbcd2c06a18fe175a64781d42e0
IC3P3 <n.eifler@freenet.de>
2026-02-09 19:23:53 +0100

    Added Skip to Content tab button

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -15,6 +15,14 @@
 </script>
 
 <div class="flex min-h-screen flex-col">
+	<a
+		href="#main-content"
+		class="sr-only focus:not-sr-only focus:fixed focus:top-2 focus:left-2 focus:z-60 focus:rounded focus:bg-blue-800 focus:px-4 focus:py-2
+  focus:text-gray-50"
+	>
+		Zum Inhalt springen
+	</a>
+
 	<Navbar {items} />
 	<main class="grow">{@render children()}</main>
 	<Footer />
