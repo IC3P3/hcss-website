697dd2d4789ab1783ed701cba28437dea2278fbc
Niklas Eifler <n.eifler@proton.me>
2026-06-18 02:43:20 +0200

    Formatting

--- src/lib/server/utils/xml_functions.ts
+++ src/lib/server/utils/xml_functions.ts
@@ -1,9 +1,11 @@
 export function escapeXML(input: string): string {
-	return input
-		// & must be replaced first, or the entities added below get double-escaped.
-		.replaceAll('&', '&amp;')
-		.replaceAll('<', '&lt;')
-		.replaceAll('>', '&gt;')
-		.replaceAll('"', '&quot;')
-		.replaceAll("'", '&apos;');
+	return (
+		input
+			// & must be replaced first, or the entities added below get double-escaped.
+			.replaceAll('&', '&amp;')
+			.replaceAll('<', '&lt;')
+			.replaceAll('>', '&gt;')
+			.replaceAll('"', '&quot;')
+			.replaceAll("'", '&apos;')
+	);
 }
