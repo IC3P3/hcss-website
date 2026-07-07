c73bceec17c9dd5a84991e8979f4b292455b8c74
Niklas Eifler <n.eifler@proton.me>
2026-07-07 17:27:10 +0200

    Added underline animation and gradient

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -14,7 +14,9 @@
 	style="background-image: url({bgImg})"
 	aria-label={img?.description || 'Platzhalter'}
 >
-	<div class="relative flex h-dvh flex-col items-center bg-black/40 bg-cover bg-center pt-16">
+	<div
+		class="relative flex h-dvh flex-col items-center bg-linear-to-b from-black/60 from-10% to-black/30 to-45% bg-cover bg-center pt-16"
+	>
 		<div class="flex flex-col items-center pt-navbar">
 			<h1 class="text-center text-6xl font-bold text-white text-shadow-lg">
 				Helmstedter Chor- und Singschule
@@ -25,10 +27,13 @@
 				Na, hallo, da will jemand mehr über die Helmstedter Chor- und Singschule erfahren.
 				Das freunt uns. Herzlich willkommen. Hier ist lernst du den Konzertchor Helmstedter
 				Chor- und Singschule kennen. Wenn du uns schon kennst aber Kontakt aufnehmen willst,
-				schreib uns gern eine <a href="mailto:HCSS38350@gmx.de" class="underline">Mail</a> oder
-				komm einfach mal vorbei. Du erreichst uns am besten zur Gesamtprobe am Freitag von 17
-				bis 20 Uhr im Gemeindehaus St. Marienberg Helmstedt. Alle anderen Zeiten findest du in
-				unserem Angebot.
+				schreib uns gern eine <a
+					href="mailto:HCSS38350@gmx.de"
+					class="relative inline-block after:absolute after:bottom-0 after:left-0 after:h-px after:w-full after:origin-left after:scale-x-0 after:bg-current after:transition-transform after:duration-300 hover:after:scale-x-100"
+					>Mail</a
+				> oder komm einfach mal vorbei. Du erreichst uns am besten zur Gesamtprobe am Freitag
+				von 17 bis 20 Uhr im Gemeindehaus St. Marienberg Helmstedt. Alle anderen Zeiten findest
+				du in unserem Angebot.
 			</p>
 			<div class="flex felx-col mt-64">
 				<a
