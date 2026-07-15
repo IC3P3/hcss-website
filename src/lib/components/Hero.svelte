139ca3d75e4a84a666ee21a1c96eb804f4bb0ddb
Niklas Eifler <n.eifler@proton.me>
2026-07-15 19:14:50 +0200

    Made the mobile view work again on hero section

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -17,27 +17,31 @@
 	<div
 		class="relative flex h-dvh flex-col items-center justify-start bg-linear-to-b from-black/70 via-black/40 to-transparent bg-cover bg-center pt-navbar"
 	>
-		<div class="flex flex-col items-center px-4 pt-navbar">
-			<h1
-				class="text-center text-4xl font-bold text-white text-shadow-lg sm:text-5xl md:text-6xl"
-			>
-				Helmstedter Chor- und Singschule
-			</h1>
-			<p
-				class="mx-5 mt-4 max-w-4xl px-2 text-center text-base text-white font-semibold text-shadow-lg sm:text-xl"
-			>
-				Willkommen. Hier ist lernst du den Konzertchor Helmstedt und die Angebote der
-				Helmstedter Chor- und Singschule kennen. Wenn du uns schon kennst aber Kontakt
-				aufnehmen willst, schreib uns gern eine <a
-					href="mailto:HCSS38350@gmx.de"
-					class="relative inline-block after:absolute after:bottom-0 after:left-0 after:h-px after:w-full after:origin-left after:scale-x-100 after:bg-current after:transition-transform after:duration-300 hover:after:scale-x-0"
-					>Mail</a
+		<div
+			class="flex h-full w-full flex-col items-center justify-between px-4 pt-8 pb-28 sm:pb-32"
+		>
+			<div class="flex flex-col items-center">
+				<h1
+					class="text-center text-4xl font-bold text-white text-shadow-lg sm:text-5xl md:text-6xl"
 				>
-				oder komm einfach mal vorbei. Du erreichst uns am besten zur Gesamtprobe am Freitag von
-				17 bis 20 Uhr im Gemeindehaus St. Marienberg Helmstedt. Alle anderen Zeiten findest du
-				in unserem Angebot.
-			</p>
-			<div class="mt-16 flex flex-col items-center sm:mt-64 sm:flex-row">
+					Helmstedter Chor- und Singschule
+				</h1>
+				<p
+					class="mx-5 mt-4 max-w-4xl px-2 text-center text-base text-white font-semibold text-shadow-lg sm:text-xl"
+				>
+					Willkommen. Hier lernst du den Konzertchor Helmstedt und die Angebote der
+					Helmstedter Chor- und Singschule kennen. Wenn du uns schon kennst aber Kontakt
+					aufnehmen willst, schreib uns gern eine <a
+						href="mailto:HCSS38350@gmx.de"
+						class="relative inline-block after:absolute after:bottom-0 after:left-0 after:h-px after:w-full after:origin-left after:scale-x-100 after:bg-current after:transition-transform after:duration-300 hover:after:scale-x-0"
+						>Mail</a
+					>
+					oder komm einfach mal vorbei. Du erreichst uns am besten zur Gesamtprobe am Freitag
+					von 17 bis 20 Uhr im Gemeindehaus St. Marienberg Helmstedt. Alle anderen Zeiten findest
+					du in unserem Angebot.
+				</p>
+			</div>
+			<div class="flex flex-col items-center sm:flex-row">
 				<a
 					href="mailto:HCSS38350@gmx.de"
 					class="mt-4 rounded-md bg-hcss-primary-700 px-7 py-3 mx-4 text-xs font-bold text-white uppercase transition-colors duration-200 hover:bg-hcss-primary-900"
