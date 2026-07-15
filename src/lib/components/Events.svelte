2159820406cb9923aca531c01e2371a12e0472f9
Niklas Eifler <n.eifler@proton.me>
2026-07-15 19:10:17 +0200

    Small rework with events

--- src/lib/components/Events.svelte
+++ src/lib/components/Events.svelte
@@ -8,70 +8,76 @@
 <section
 	id="veranstaltungen"
 	aria-labelledby="events-heading"
-	class="mx-auto w-full max-w-7xl justify-center p-4 py-6 text-center lg:py-8"
+	class="w-full bg-linear-to-b from-white via-hcss-primary-100 to-white"
 >
-	<h2 id="events-heading" class="mb-8 text-4xl font-extrabold text-hcss-primary-950">Konzerte</h2>
-	<hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+	<div class="mx-auto w-full max-w-7xl p-4 py-6 text-center lg:py-8">
+		<h2 id="events-heading" class="mb-8 text-4xl font-extrabold text-hcss-primary-950">
+			Konzerte
+		</h2>
+		<hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
 
-	{#if events.length !== 0}
-		<ul aria-label="Liste der Konzerte" class="list-none divide-y divide-gray-200 p-0">
-			{#each events as event (event.id)}
-				{@const date = new Date(event.time)}
-				<li class="py-4">
-					<article class="my-4 rounded-lg bg-white p-6">
-						<div class="gap-4 lg:grid lg:grid-cols-4 lg:gap-8">
-							<div class="mt-1 lg:flex lg:flex-col lg:justify-center">
-								<time
-									datetime={date.toISOString()}
-									class="block text-left text-xl font-semibold text-hcss-primary-950"
+		{#if events.length !== 0}
+			<ul aria-label="Liste der Konzerte" class="list-none divide-y divide-gray-200 p-0">
+				{#each events as event (event.id)}
+					{@const date = new Date(event.time)}
+					<li class="py-4">
+						<article class="my-4 rounded-lg bg-white p-6">
+							<div class="gap-4 lg:grid lg:grid-cols-4 lg:gap-8">
+								<div class="mt-1 lg:flex lg:flex-col lg:justify-center">
+									<time
+										datetime={date.toISOString()}
+										class="block text-left text-xl font-semibold text-hcss-primary-950"
+									>
+										{date.toLocaleDateString('de', {
+											day: '2-digit',
+											month: 'long',
+											year: 'numeric'
+										})}
+									</time>
+									<p class="text-left text-lg text-gray-600">
+										{date.toLocaleTimeString('de', {
+											hour: '2-digit',
+											minute: '2-digit'
+										})} Uhr
+									</p>
+								</div>
+
+								<div class="mt-4 lg:col-span-2 lg:mt-0 lg:text-center">
+									<h3 class="mb-2 text-2xl font-bold text-hcss-primary-950">
+										{event.title}
+									</h3>
+									{#if event.shortDescription}
+										<p class="text-lg text-gray-700">
+											{event.shortDescription}
+										</p>
+									{/if}
+								</div>
+
+								<address
+									class="mt-4 not-italic lg:mt-0 lg:flex lg:flex-col lg:justify-center lg:text-right"
 								>
-									{date.toLocaleDateString('de', {
-										day: '2-digit',
-										month: 'long',
-										year: 'numeric'
-									})}
-								</time>
-								<p class="text-left text-lg text-gray-600">
-									{date.toLocaleTimeString('de', {
-										hour: '2-digit',
-										minute: '2-digit'
-									})} Uhr
-								</p>
+									{#each event.address.split(', ') as part, index (index)}
+										<p class="text-lg text-gray-700">{part}</p>
+									{/each}
+								</address>
 							</div>
-
-							<div class="mt-4 lg:col-span-2 lg:mt-0 lg:text-center">
-								<h3 class="mb-2 text-2xl font-bold text-hcss-primary-950">
-									{event.title}
-								</h3>
-								{#if event.shortDescription}
-									<p class="text-lg text-gray-700">{event.shortDescription}</p>
-								{/if}
-							</div>
-
-							<address
-								class="mt-4 not-italic lg:mt-0 lg:flex lg:flex-col lg:justify-center lg:text-right"
-							>
-								{#each event.address.split(', ') as part, index (index)}
-									<p class="text-lg text-gray-700">{part}</p>
-								{/each}
-							</address>
-						</div>
-					</article>
-				</li>
-			{/each}
-		</ul>
-	{:else}
-		<div class="my-4 rounded-lg border border-gray-200 bg-white p-10 shadow-md">
-			<p class="text-xl text-gray-700">Momentan sind keine Konzerte geplant.</p>
-			<p class="mt-2 text-lg text-gray-500">
-				Folgt uns auf Social Media, um keine Neuigkeiten zu verpassen!
-			</p>
-			<div class="mt-6 flex justify-center">
-				<SocialMedia
-					color="text-hcss-primary-950"
-					hoverColor="hover:text-hcss-primary-700"
-				/>
+						</article>
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<div class="my-4 rounded-lg border border-gray-200 bg-white p-10 shadow-md">
+				<p class="text-xl text-gray-700">Momentan sind keine Konzerte geplant.</p>
+				<p class="mt-2 text-lg text-gray-500">
+					Folgt uns auf Social Media, um keine Neuigkeiten zu verpassen!
+				</p>
+				<div class="mt-6 flex justify-center">
+					<SocialMedia
+						color="text-hcss-primary-950"
+						hoverColor="hover:text-hcss-primary-700"
+					/>
+				</div>
 			</div>
-		</div>
-	{/if}
+		{/if}
+	</div>
 </section>
